8ad4c6a67d6a32a56103b99ae5227a80f6eb5503
MBeshkov <u05mb19@abdn.ac.uk>
2024-02-25 09:03:08 +0000

    modified:   procurement-comparison/src/ProductList.css 	modified:   procurement-comparison/src/ProductList.js

--- procurement-comparison/src/ProductList.js
+++ procurement-comparison/src/ProductList.js
@@ -11,18 +11,45 @@ function ProductList() {
   useEffect(() => {
     // Make a request to your Django API endpoint
     axios.get('http://localhost:8000/products/api')  // Use the full URL with the /api endpoint
-      .then(response => setProducts(response.data))
+      .then(response => {
+        // Fetch additional details (including image) for each product
+        const productsWithDetails = response.data.map(async product => {
+          const apiProduct = await fetchApiProductDetails(product.code);
+          return {
+            ...product,
+            img_url: apiProduct.image_front_small_url,
+          };
+        });
+
+        // Wait for all details to be fetched before updating state
+        Promise.all(productsWithDetails)
+          .then(updatedProducts => setProducts(updatedProducts))
+          .catch(error => console.error('Error fetching product details:', error));
+      })
       .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  // Function to fetch product details from the API
+  const fetchApiProductDetails = async (code) => {
+    try {
+      const apiUrl = `https://uk.openfoodfacts.org/api/v2/product/${code}?fields=image_front_small_url`;
+      const response = await axios.get(apiUrl);
+      return response.data.product;
+    } catch (error) {
+      console.error('Error fetching API data:', error);
+      return {};
+    }
+  };
+
   return (
-    <div className="centered-list"> {/* Apply the CSS class to center the content */}
-      <h2>All Products</h2>
+    <div className="centered-list">
+      <h2><br></br>All Products</h2>
       <ul>
         {products.map(product => (
           <li key={product.code}>
             <Link to={`/products/${product.code}`}>
-              <strong>{product.name}</strong> - Eco-score: {product.score}
+            {product.img_url && <img src={product.img_url} alt={`Product ${product.name}`} className="product-image" />}
+            <strong>{product.name}</strong> ECO-SCORE: {product.score}
             </Link>
           </li>
         ))}
